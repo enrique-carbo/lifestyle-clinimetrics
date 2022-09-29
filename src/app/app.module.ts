e36ae0a69babb28694990e6c61c89bd38f315b3c
ecarbo <enriquecarbo@tutanota.com>
2022-09-29 13:57:19 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Components
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BodyMassIndexComponent } from './pages/body-mass-index/body-mass-index.component'
+import {SidenavListComponent} from './component/sidenav-list/sidenav-list.component';
+
+// Angular Material
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input'
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SidenavListComponent,
+    BodyMassIndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/body-mass-index/body-mass-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-mass-index',
+  templateUrl: './body-mass-index.component.html',
+  styleUrls: ['./body-mass-index.component.css']
+})
+export class BodyMassIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
